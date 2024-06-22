e6af246af19f38629bceec22b37c055c398be161
Linus <flyrhinosfly@gmail.com>
2024-06-21 21:08:44 -0400

    simple imput output provided by play()

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-import chopsticks
-import init
-import files
-import play
+from play import play
 
-X=123
-position_list = init.Fill_list()
-
-print(len(position_list))
-init.Link_list(position_list)
-print(str(position_list[X].index) + "\t" + str(position_list[X]))
-for x in position_list[X].flinks:
-    print(str(x))
-#files.write(position_list)
-play.play()
+play()
--- play.py
+++ play.py
@@ -1,4 +1,21 @@
-#the interactive shell
+#THE INTERACTIVE SHELL
+#simple input output capability
+
+def startup():
+    print("Do you want to play a game?")
+    while input_string := input("y/n: "):
+        if input_string == 'n':
+            print("too bad...")
+            return 1
+        if input_string == 'y':
+            print("excellent...")
+            return 0
+        else:
+            continue
 
 def play():
-    print("now were playing a game!")
+    if startup() == 1:
+        return 0
+    while input_string := input("> "):
+        print(input_string)
+    return 0
